4bd7c3fea8728f81c73a6e836ace7cf8748a25b4
Aman Rajpoot <amanrajpoot59@gmail.com>
2025-06-28 20:00:14 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,8 @@ export default function Document() {
           content="agency, bootstrap 5, business, business multipurpose, charity, creative, creative template, crypto, education, hosting, insurance, landing page, portfolio, real estate, responsive, react, nextjs"
         />
 
+        <meta name="fast2sms" content="zZGcF6194OWJmuoHxOWrOlnusxY6qsBm">
+
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
